baa70f943911e528fd3f4d40d25cf79d272402ca
Thiago Santos <thia.mdossantos@gmail.com>
2017-07-23 11:03:29 -0300

    add more exercises

---
+++ exercise-images.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+  "golang.org/x/tour/pic"
+  "image/color"
+  "image"
+)
+
+type Image struct{
+  Width int
+  Height int
+}
+
+func (image Image) ColorModel() color.Model {
+  return color.RGBAModel
+}
+
+func (img Image) Bounds() image.Rectangle {
+  return image.Rect(0, 0, img.Width, img.Height)
+}
+
+func (img Image) At(x, y int) color.Color {
+    return color.RGBA{21, 21, 21, 255}
+}
+
+func main() {
+  m := Image{12, 12}
+  pic.ShowImage(m)
+}
